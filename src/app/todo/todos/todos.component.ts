189db9485e4e449e279ce824a1fb33e2ddaebdf3
Mingyu Lee <leemingyu05@gmail.com>
2017-08-28 13:33:13 +0900

    todo component 개발 진행

--- src/app/todo/todos/todos.component.ts
+++ src/app/todo/todos/todos.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-// ES6의 Module
+// ES6의 Component, metadata
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
+  selector: 'app-todos', // css 선택자처럼 사용 가능, 현재는 엘리먼트 이름이 app-todos인 DOM 요소를 선택
+  templateUrl: './todos.component.html', // 뷰에 대한 정의 템플릿 HTML
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
